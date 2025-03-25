aa3cc9fb53b37e1e11019c135d9dc4735c7f04b0
TEDEU <113966487+kawa0929@users.noreply.github.com>
2025-03-25 11:49:55 +0800

    v2(time修改)

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    homepage = "<h1>許閔慈Python網頁</h1>"
+    homepage = "<h1>許閔慈Python網頁(時間+8)</h1>"
     homepage += "<a href=/mis>MIS</a><br>"
     homepage += "<a href=/today>顯示日期時間</a><br>"
     homepage += "<a href=/welcome?nick=許閔慈&work=靜宜資管>傳送使用者暱稱</a><br>"
